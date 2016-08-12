e7067ce66066f154ba1e3d861a702db53b5f0eea
Tom <thomas.verin@sonarsource.com>
2016-08-12 14:37:53 +0200

    handle release version in QA cix.sh

--- cix.sh
+++ cix.sh
@@ -4,9 +4,18 @@ echo "Running for $CI_BUILD_NUMBER"
   
   #deploy the version built by travis
   CURRENT_VERSION=`mvn help:evaluate -Dexpression="project.version" | grep -v '^\[\|Download\w\+\:'`
-  RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
-  NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
-  echo $NEW_VERSION
+
+  if [[ $CURRENT_VERSION =~ "-SNAPSHOT" ]]; then
+    echo "======= Found SNAPSHOT version ======="
+    # Do not deploy a SNAPSHOT version but the release version related to this build
+    RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
+    NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
+    echo $NEW_VERSION  
+  else
+    echo "======= Found RELEASE version ======="
+    NEW_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
+  fi
+
   mkdir -p target
   cd target
   curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/sonarsource/sonarlint/sonarlint-cli/$NEW_VERSION/sonarlint-cli-$NEW_VERSION.zip

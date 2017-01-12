f654e9991f9bcd02412751ab7f9d4e14c036f535
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-01-12 14:38:21 +0100

    Upgrade parent version and update license

--- src/test/java/org/sonarlint/cli/TestUtils.java
+++ src/test/java/org/sonarlint/cli/TestUtils.java
@@ -1,7 +1,7 @@
 /*
  * SonarLint CLI
- * Copyright (C) 2016-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
+ * Copyright (C) 2016-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
